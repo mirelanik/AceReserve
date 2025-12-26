66af84f3a14d5d2795a7ef9fb5c2b7474509dc02
mirelanik <mirelanik18@gmail.com>
2025-12-27 01:12:14 +0200

    Add main.py and database setup

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from .database import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
